45a71bf41d58d3ca187b4c117f9f1a3c7fb9ac6f
Francesco Grillea <grilleafrancesco@gmail.com>
2024-06-01 18:46:53 +0200

    load 3d dataset

--- matlab/utility/loadDataset.m
+++ matlab/utility/loadDataset.m
@@ -13,6 +13,27 @@ function [trainingSet, trainingLabels, testSet, testLabels] = loadDataset(datase
         trainingLabels = datasetStruct.training.labels;
         testSet = reshape(datasetStruct.test.images, 28, 28, 1, []);
         testLabels = datasetStruct.test.labels;
+    elseif strcmp(dataset, 'mnist3d')
+        datasetStruct = load("datasets/mnist.mat");
+        trainingSet = reshape(datasetStruct.training.images, 28, 28, 1, []);
+        trainingLabels = datasetStruct.training.labels;
+        n = size(trainingSet, 4);
+        n_new_channels = 2;
+        for i=1:n_new_channels
+            channel = randn(28, 28, 1, n);
+            channel = normalize(channel, "range");
+            trainingSet = cat(3, trainingSet, channel);
+        end
+
+        testSet = reshape(datasetStruct.test.images, 28, 28, 1, []);
+        testLabels = datasetStruct.test.labels;
+        n = size(testSet, 4);
+        for i=1:n_new_channels
+            channel = randn(28, 28, 1, n);
+            channel = normalize(channel, "range");
+            testSet = cat(3, testSet, channel);
+        end
+
 
     elseif strcmp(dataset, 'lognorm') | strcmp(dataset, "overlapping_lognorm") | strcmp(dataset, 'lognorm_rescaled')
         datasetStruct = load(sprintf("datasets/%s.mat", dataset));
